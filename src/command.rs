102e36eac8ff5a87508233423d27bd6769ce932d
dastansam <dastan.samatov.33@gmail.com>
2025-02-24 16:44:50 +0600

    Compiling RPC

--- src/command.rs
+++ src/command.rs
@@ -1,8 +1,5 @@
 //! Command line interface for the Subeth RPC adapter.
 
-use crate::sub_client::SubLightClient;
-use clap::Parser;
-
 #[derive(clap::Parser)]
 #[clap(version = "0.1", author = "Subeth")]
 struct Opts {
