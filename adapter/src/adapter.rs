2bdce513c0b8cb65d204ca57c3f3dddb626982f2
Dastan <88332432+dastansam@users.noreply.github.com>
2025-11-22 00:32:43 +0600

    Adapter changes

---
+++ adapter/src/adapter.rs
@@ -0,0 +1,183 @@
+//! Types for converting ETH-like types to Substrate types and vice versa
+
+use alloy_primitives::Address;
+use sp_core::{blake2_128, blake2_256, twox_128, twox_256, twox_64};
+use subxt::{metadata::types::StorageHasher, utils::AccountId32};
+
+/// Reserved address for generic SCALE-encoded RuntimeCall dispatch
+/// When EthereumTransaction.to == GENERIC_CALL_ADDRESS, the transaction.data
+/// field contains a SCALE-encoded RuntimeCall that will be decoded and dispatched.
+pub const GENERIC_CALL_ADDRESS: Address = Address::ZERO;
+
+/// Pallet to contract address mapping
+pub(crate) struct PalletContractMapping;
+
+impl PalletContractMapping {
+    /// Get the contract address for a given pallet
+    pub fn contract_address(pallet: &str) -> Address {
+        // take only 8 chars from the pallet name
+        let prefix = pallet.chars().take(8).collect::<String>();
+        Self::pad_to_eth_address(&prefix)
+    }
+
+    /// Get the pallet name for a given contract address
+    pub fn pallet_name(address: Address) -> Option<String> {
+        // try to convert the address to a string
+        let prefix = String::from_utf8(address.to_vec()).ok()?;
+        Some(prefix.trim_end_matches('\0').to_string())
+    }
+
+    /// Pad the given string to a valid Ethereum address (20 bytes)
+    fn pad_to_eth_address(prefix: &str) -> Address {
+        let mut address = [0u8; 20];
+        let prefix_bytes = prefix.as_bytes();
+        let len = prefix_bytes.len().min(20);
+        address[..len].copy_from_slice(&prefix_bytes[..len]);
+        Address::from(address)
+    }
+}
+/// Address mapping logic
+pub(crate) struct AddressMapping;
+
+impl AddressMapping {
+    /// Hash `AccountId20` to get `AccountId32`
+    pub fn to_ss58(address: Address) -> AccountId32 {
+        let mut input = [0u8; 32];
+        input[..20].copy_from_slice(&address.to_vec());
+        let hash = blake2_256(&input);
+        AccountId32::from(hash)
+    }
+
+    /// Truncate `AccountId32` to get `AccountId20`
+    pub fn to_address(account_id: AccountId32) -> Address {
+        let inner: &[u8; 32] = account_id.as_ref();
+        Address::from_slice(&inner[..20])
+    }
+}
+
+/// Pallet storage read structure
+#[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
+pub struct StorageKey {
+    /// Storage item name
+    pub name: String,
+    /// The rest of the keys, could be multiple if n-map storage
+    pub keys: Vec<Vec<u8>>,
+}
+
+/// Hash the key to get the storage key
+pub fn hash_key(key: &[u8], hasher: &StorageHasher) -> Vec<u8> {
+    match hasher {
+        StorageHasher::Blake2_128 => blake2_128(key).to_vec(),
+        StorageHasher::Blake2_256 => blake2_256(key).to_vec(),
+        StorageHasher::Blake2_128Concat => {
+            let hash = blake2_128(key);
+            let mut result = Vec::with_capacity(32 + key.len());
+            result.extend_from_slice(&hash);
+            result.extend_from_slice(key);
+            result
+        }
+        StorageHasher::Twox128 => twox_128(key).to_vec(),
+        StorageHasher::Twox256 => twox_256(key).to_vec(),
+        StorageHasher::Twox64Concat => {
+            let hash = twox_64(key);
+            let mut result = Vec::with_capacity(8 + key.len());
+            result.extend_from_slice(&hash);
+            result.extend_from_slice(key);
+            result
+        }
+        StorageHasher::Identity => key.to_vec(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_storage_key_works() {
+        let key = StorageKey {
+            name: "Account".to_string(),
+            keys: vec![hex::decode(
+                "5e51610a8f5b5e04bf2f28960aa138df2b7531d0dbea936336ed40a204c92e12",
+            )
+            .unwrap()],
+        };
+
+        assert_eq!(
+            serde_json::to_string(&key).unwrap(),
+            r#"{"name":"Account","keys":[[94,81,97,10,143,91,94,4,191,47,40,150,10,161,56,223,43,117,49,208,219,234,147,99,54,237,64,162,4,201,46,18]]}"#
+        );
+
+        let key = StorageKey {
+            name: "ActiveEra".to_string(),
+            keys: vec![],
+        };
+
+        assert_eq!(
+            serde_json::to_string(&key).unwrap(),
+            r#"{"name":"ActiveEra","keys":[]}"#
+        );
+    }
+
+    #[test]
+    fn test_hash_and_truncate() {
+        let address = Address::from([1u8; 20]);
+        let expected_account_id =
+            hex::decode("8b304616ddedac8267d0381d53301825902eb056a70fc56b90e84efa492a015b").unwrap();
+        let account_id = AddressMapping::to_ss58(address);
+        let account_id_raw: &[u8] = account_id.as_ref();
+        assert_eq!(account_id_raw, expected_account_id.as_slice());
+
+        let account_id = AccountId32::from([1u8; 32]);
+        let new_address = AddressMapping::to_address(account_id);
+
+        assert_eq!(address, new_address);
+    }
+
+    #[test]
+    fn test_pallet_mapping_works() {
+        let balances = PalletContractMapping::contract_address("Balances");
+        let system = PalletContractMapping::contract_address("System");
+        let staking = PalletContractMapping::contract_address("Staking");
+        let democracy = PalletContractMapping::contract_address("democrac");
+        let treasury = PalletContractMapping::contract_address("treasury");
+
+        assert_eq!(
+            system,
+            "0x53797374656d0000000000000000000000000000".parse::<Address>().unwrap()
+        );
+        assert_eq!(
+            balances,
+            "0x42616c616e636573000000000000000000000000".parse::<Address>().unwrap()
+        );
+        assert_eq!(
+            staking,
+            "0x5374616b696e6700000000000000000000000000".parse::<Address>().unwrap()
+        );
+        assert_eq!(
+            democracy,
+            "0x64656d6f63726163000000000000000000000000".parse::<Address>().unwrap()
+        );
+        assert_eq!(
+            treasury,
+            "0x7472656173757279000000000000000000000000".parse::<Address>().unwrap()
+        );
+
+        assert_eq!(
+            PalletContractMapping::pallet_name(balances).unwrap(),
+            "Balances"
+        );
+        assert_eq!(
+            PalletContractMapping::pallet_name(staking).unwrap(),
+            "Staking"
+        );
+        assert_eq!(
+            PalletContractMapping::pallet_name(democracy).unwrap(),
+            "democrac"
+        );
+        assert_eq!(
+            PalletContractMapping::pallet_name(treasury).unwrap(),
+            "treasury"
+        );
+    }
+}
---
+++ adapter/src/command.rs
@@ -0,0 +1,147 @@
+//! Command line interface for the Subeth RPC adapter.
+
+use std::sync::Arc;
+
+use jsonrpsee::RpcModule;
+use sc_service::config::RpcConfiguration;
+
+use crate::{sub_client::SubLightClient, traits::EthApiServer};
+use std::{
+    cell::RefCell,
+    sync::atomic::{AtomicUsize, Ordering},
+};
+
+const DEFAULT_URL: &str = "wss://rpc.ibp.network/polkadot";
+
+#[derive(clap::Parser, Debug)]
+#[clap(version = "0.1", author = "Subeth")]
+pub struct Opts {
+    /// Chain specification file
+    #[clap(short, long)]
+    chain_spec: Option<String>,
+    /// Remote node URL
+    #[clap(short, long)]
+    url: Option<String>,
+    /// Chain ID
+    #[clap(long, default_value = "42")]
+    chain_id: u64,
+    /// Maximum retries for light client initialization
+    #[clap(long, default_value = "3")]
+    max_retries: u32,
+    /// Rpc params
+    #[allow(missing_docs)]
+    #[clap(flatten)]
+    rpc_params: sc_cli::RpcParams,
+}
+
+/// Blocks current thread until ctrl-c is received
+pub async fn block_until_sigint() {
+    let (ctrlc_send, ctrlc_oneshot) = futures::channel::oneshot::channel();
+    let ctrlc_send_c = RefCell::new(Some(ctrlc_send));
+
+    let running = Arc::new(AtomicUsize::new(0));
+    ctrlc::set_handler(move || {
+        let prev = running.fetch_add(1, Ordering::SeqCst);
+        if prev == 0 {
+            println!("Got interrupt, shutting down...");
+            // Send sig int in channel to blocking task
+            if let Some(ctrlc_send) = ctrlc_send_c.try_borrow_mut().unwrap().take() {
+                ctrlc_send.send(()).expect("Error sending ctrl-c message");
+            }
+        } else {
+            std::process::exit(0);
+        }
+    })
+    .expect("Error setting Ctrl-C handler");
+
+    ctrlc_oneshot.await.unwrap();
+}
+
+fn tokio_runtime() -> Result<tokio::runtime::Runtime, tokio::io::Error> {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+}
+
+pub async fn run(opts: Opts) -> anyhow::Result<()> {
+    let chain_id = opts.chain_id;
+
+    // figure out if we are relying on a smoldot node or RPC node
+    let client = if let Some(chain_spec_path) = opts.chain_spec {
+        // load the chain spec file
+        log::info!("Loading chain spec from: {}", chain_spec_path);
+        let chain_spec = std::fs::read_to_string(&chain_spec_path)?;
+
+        SubLightClient::from_light_client(&chain_spec, chain_id, None).await?
+    } else if let Some(url) = opts.url {
+        // create a new RPC client
+        SubLightClient::from_url(&url, chain_id, None).await?
+    } else {
+        // default to a Polkadot node
+        // let polkadot_spec = include_str!("../specs/polkadot.json");
+        // SubLightClient::from_light_client(polkadot_spec, chain_id, None).await?
+        SubLightClient::from_url(DEFAULT_URL, chain_id, None).await?
+    };
+
+    log::info!("Connected to chain: {}", client.chain_id());
+
+    let tokio_runtime = tokio_runtime()?;
+    let tokio_handle = tokio_runtime.handle();
+
+    let mut task_manager = sc_service::TaskManager::new(tokio_handle.clone(), None)?;
+
+    let mut gen_rpc_module = RpcModule::new(());
+    let rpc_module =
+        crate::server::EthAdapter::new(client, vec![], Arc::new(task_manager.spawn_handle()))
+            .into_rpc();
+
+    gen_rpc_module
+        .merge(rpc_module)
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    let rpc_port = opts.rpc_params.rpc_port.unwrap_or(8545);
+    log::info!("RPC port: {}", rpc_port);
+
+    let addrs: Option<Vec<sc_service::config::RpcEndpoint>> = opts
+        .rpc_params
+        .rpc_addr(false, false, rpc_port)?
+        .map(|addrs| addrs.into_iter().map(Into::into).collect());
+
+    log::info!("Launching RPC server at: {:?}", addrs);
+
+    let rpc_params = opts.rpc_params;
+
+    let rpc_config = RpcConfiguration {
+        addr: addrs,
+        methods: rpc_params.rpc_methods.into(),
+        max_connections: rpc_params.rpc_max_connections,
+        cors: rpc_params.rpc_cors(true)?,
+        max_request_size: rpc_params.rpc_max_request_size,
+        max_response_size: rpc_params.rpc_max_response_size,
+        id_provider: None,
+        max_subs_per_conn: rpc_params.rpc_max_subscriptions_per_connection,
+        port: rpc_params.rpc_port.unwrap_or(8545),
+        message_buffer_capacity: rpc_params.rpc_message_buffer_capacity_per_connection,
+        batch_config: rpc_params.rpc_batch_config()?,
+        rate_limit: rpc_params.rpc_rate_limit,
+        rate_limit_whitelisted_ips: rpc_params.rpc_rate_limit_whitelisted_ips,
+        rate_limit_trust_proxy_headers: rpc_params.rpc_rate_limit_trust_proxy_headers,
+    };
+    let eth_rpc_handle = sc_service::start_rpc_servers(
+        &rpc_config,
+        None,
+        tokio_handle,
+        || Ok(gen_rpc_module.clone()),
+        None,
+    )?;
+
+    task_manager.keep_alive(eth_rpc_handle);
+
+    // // block until ctrl-c is received
+    // let signals = tokio_runtime.block_on(async { sc_cli::Signals::capture() })?;
+    // tokio_runtime.block_on(signals.run_until_signal(task_manager.future().fuse()))?;
+    block_until_sigint().await;
+
+    tokio_runtime.shutdown_timeout(std::time::Duration::from_secs(10));
+    Ok(())
+}
